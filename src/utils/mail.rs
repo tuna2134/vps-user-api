4fd689e85bfdb81536d2035258a86c340641fedc
tuna2134 <hello@tuna2134.dev>
2025-08-14 15:47:56 +0900

    refactor: format debug logging for email message creation in send_passcode function

--- src/utils/mail.rs
+++ src/utils/mail.rs
@@ -14,7 +14,10 @@ pub async fn send_passcode(code: String, mail_to: String) -> anyhow::Result<()> 
         .header(ContentType::TEXT_PLAIN)
         .body(format!("Your passcode is: {}", code))?;
 
-    tracing::debug!("Email message created: {:?}", env::var("SMTP_HOSTNAME")?.as_str());
+    tracing::debug!(
+        "Email message created: {:?}",
+        env::var("SMTP_HOSTNAME")?.as_str()
+    );
     let creds = Credentials::new(env::var("SMTP_USERNAME")?, env::var("SMTP_PASSWORD")?);
     let mailer: AsyncSmtpTransport<Tokio1Executor> =
         AsyncSmtpTransport::<Tokio1Executor>::starttls_relay(&env::var("SMTP_HOSTNAME")?)?
